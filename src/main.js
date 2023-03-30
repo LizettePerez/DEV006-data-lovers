91e13dbeef019faee64079f4557c529babcf1870
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-03-30 13:45:30 -0300

    update img

--- src/main.js
+++ src/main.js
@@ -111,6 +111,19 @@ function searchPokemon() {
   }
 }
 
+// Obtén la referencia del botón "arriba"
+const botonArriba = document.querySelector('.arriba');
+
+// Agrega un listener de evento al botón "arriba"
+botonArriba.addEventListener('click', function () {
+  // Usa la función "scrollTo" para moverte al inicio del documento
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
 /* eslint-disable no-console */
 console.log(data);
 /* eslint-enable no-console */
