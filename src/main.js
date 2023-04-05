143dd91a8357cf8067bf409fcbd80d62cfcbbfc2
Jimena92 <jimena.flores.92@gmail.com>
2023-04-05 07:25:20 -0600

    filtro en comentarios

--- src/main.js
+++ src/main.js
@@ -119,6 +119,126 @@ function myFunction() {
   }
 }
 
+// const sortSelect = document.querySelector("#sort-select");
+// sortSelect.addEventListener("change", sortPokemon);
+
+// function sortPokemon() {
+//   const sortValue = sortSelect.value;
+//   const cards = Array.from(document.querySelectorAll(".pokemon-card"));
+
+//   cards.sort((a, b) => {
+//     const aData = data.pokemon.find(
+//       (p) => p.name === a.querySelector("h2").textContent.toLowerCase()
+//     );
+//     const bData = data.pokemon.find(
+//       (p) => p.name === b.querySelector("h2").textContent.toLowerCase()
+//     );
+
+//     switch (sortValue) {
+//       case "name-asc":
+//         return aData.name.localeCompare(bData.name);
+//       case "name-desc":
+//         return bData.name.localeCompare(aData.name);
+//       case "num-asc":
+//         return aData.num.localeCompare(bData.num);
+//       case "num-desc":
+//         return bData.num.localeCompare(aData.num);
+//       default:
+//         return 0;
+//     }
+//   });
+
+//   pokemonContainer.innerHTML = "";
+//   cards.forEach((card) => {
+//     pokemonContainer.appendChild(card);
+//   });
+// }const sortSelect = document.querySelector("#sort-select");
+sortSelect.addEventListener("change", sortPokemon);
+
+function sortPokemon() {
+  const sortValue = sortSelect.value;
+  const cards = Array.from(document.querySelectorAll(".pokemon-card"));
+
+  cards.sort((a, b) => {
+    const aData = data.pokemon.find(
+      (p) => p.name === a.querySelector("h2").textContent.toLowerCase()
+    );
+    const bData = data.pokemon.find(
+      (p) => p.name === b.querySelector("h2").textContent.toLowerCase()
+    );
+
+    switch (sortValue) {
+      case "name-asc":
+        return aData.name.localeCompare(bData.name);
+      case "name-desc":
+        return bData.name.localeCompare(aData.name);
+      case "num-asc":
+        return aData.num.localeCompare(bData.num);
+      case "num-desc":
+        return bData.num.localeCompare(aData.num);
+      default:
+        return 0;
+    }
+  });
+
+  pokemonContainer.innerHTML = "";
+  cards.forEach((card) => {
+    pokemonContainer.appendChild(card);
+  });
+}
+
+// // Obtener el elemento de selección de orden
+// const filterSelect = document.querySelector("#sort-select");
+
+// // Agregar un event listener al elemento de selección de orden
+// filterSelect.addEventListener("change", filterPokemon);
+
+// // Función de ordenamiento
+// function filterPokemon() {
+//   // Obtener el valor de ordenamiento seleccionado por el usuario
+//   const filterValue = filterSelect.value;
+
+//   // Obtener todos los elementos de la tarjeta Pokemon
+//   const cards = document.querySelectorAll(".pokemon-card");
+
+//   // Convertir los elementos de la tarjeta Pokemon a un array
+//   const pokemonArray = Array.from(cards);
+
+//   // Ordenar el array según el valor de ordenamiento seleccionado por el usuario
+//   if (filterValue === "num-asc") {
+//     pokemonArray.sort((a, b) => {
+//       return (
+//         parseInt(a.querySelector("p").textContent.slice(1)) -
+//         parseInt(b.querySelector("p").textContent.slice(1))
+//       );
+//     });
+//   } else if (filterValue === "num-desc") {
+//     pokemonArray.sort((a, b) => {
+//       return (
+//         parseInt(b.querySelector("p").textContent.slice(1)) -
+//         parseInt(a.querySelector("p").textContent.slice(1))
+//       );
+//     });
+//   } else if (filterValue === "name-asc") {
+//     pokemonArray.sort((a, b) => {
+//       return a
+//         .querySelector("h2")
+//         .textContent.localeCompare(b.querySelector("h2").textContent);
+//     });
+//   } else if (filterValue === "name-desc") {
+//     pokemonArray.sort((a, b) => {
+//       return b
+//         .querySelector("h2")
+//         .textContent.localeCompare(a.querySelector("h2").textContent);
+//     });
+//   }
+
+//   // Recorrer el array ordenado y agregar cada elemento a la página
+//   for (let i = 0; i < pokemonArray.length; i++) {
+//     pokemonContainer.appendChild(pokemonArray[i]);
+//   }
+// }
+
 /* eslint-disable no-console */
 console.log(data);
 /* eslint-enable no-console */
